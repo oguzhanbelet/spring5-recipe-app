c1e82e6e1e2323c22413cd1a29eca28dce27e95a
Belet <oguzhan.belet@solviads.com>
2022-04-08 13:11:29 +0300

    Entities and Controller added.

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        System.out.println("say something 5555555");
         return "index";
     }
 
