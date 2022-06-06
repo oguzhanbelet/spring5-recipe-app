b2f4b23b0ef91b1edca4e23953d7868f27d6c6a2
Belet <oguzhan.belet@solviads.com>
2022-06-06 21:43:43 +0300

    adds recipe findById

--- src/main/java/guru/springframework/spring5recipeapp/service/impl/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/service/impl/RecipeServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -26,5 +27,15 @@ public class RecipeServiceImpl implements RecipeService {
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (!recipeOptional.isPresent()){
+            throw new RuntimeException("Recipe not found!");
+        }
+        return recipeOptional.get();
     }
 }
